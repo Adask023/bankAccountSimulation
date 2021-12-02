5e684030f9bbf99a81959cd1d5582ae6c8574ae4
Adam Kudłacik <adam.kud@wp.pl>
2021-12-02 02:53:21 +0100

    add validation logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators, State } from "./state";
+import { useState } from "react";
+import { ActionType } from "./state/action-types";
 
 function App() {
   const dispatch = useDispatch();
+  const [bankInput, setBankInput] = useState<number>(0);
+  const [personName, setPersonName] = useState<string>("");
+  const [errors, setErrors] = useState<string[]>([]);
 
   const { depositMoney, withdrawMoney, bankruptMoney } = bindActionCreators(
     actionCreators,
@@ -14,19 +17,77 @@ function App() {
   );
   const amount = useSelector((state: State) => state.bank);
 
+  const addError = (err: string) => {
+    console.log(err);
+    if (errors.length > 0 && !errors.includes(err)) {
+      setErrors([...errors, err]);
+    } else {
+      setErrors([err]);
+    }
+  };
+
+  const checkForm = (action: string) => {
+    let errCount = 0;
+    if (action === ActionType.DEPOSIT && bankInput > 10000) {
+      addError("No joking, you can't have that much money!");
+      errCount++;
+    }
+    if (action === ActionType.WITHDRAW && amount - bankInput < 0) {
+      addError("You can't withdraw money that you don't have!");
+      errCount++;
+    }
+    if(personName.length === 0){
+      addError("Enter a name");
+      errCount++;
+    }
+    if(personName.length === 0){
+      addError("Name is to short, at least 2 characters");
+      errCount++;
+    }
+    if (errCount) {
+      return false;
+    } else {
+      setErrors([]);
+      return true;
+    }
+  };
+
+  const handleDeposit = () => {
+    if (checkForm(ActionType.DEPOSIT)) {
+      depositMoney(bankInput);
+      setBankInput(0);
+    }
+  };
+
+  const handleWithdraw = () => {
+    if (checkForm(ActionType.WITHDRAW)) {
+      withdrawMoney(bankInput);
+      setBankInput(0);
+    }
+  };
+
   return (
     <div className="App">
       <h1>{amount}</h1>
+      {errors && (
+        <div>
+          {errors.map((err) => (
+            <p key={Math.floor(Math.random() * 1000)}>{err}</p>
+          ))}
+        </div>
+      )}
       <button
+        disabled={!bankInput}
         onClick={() => {
-          depositMoney(1000);
+          handleDeposit();
         }}
       >
         Deposit
       </button>
       <button
+        disabled={!bankInput}
         onClick={() => {
-          withdrawMoney(1000);
+          handleWithdraw();
         }}
       >
         Withdraw
@@ -34,10 +95,25 @@ function App() {
       <button
         onClick={() => {
           bankruptMoney();
+          setErrors([]);
         }}
       >
         Bankrupt
       </button>
+      <br />
+      How much:
+      <input
+        type="number"
+        value={bankInput}
+        onChange={(e) => setBankInput(Number(e.target.value))}
+      />
+      <br />
+      Name:
+      <input
+        type="text"
+        value={personName}
+        onChange={(e) => setPersonName(e.target.value)}
+      />
     </div>
   );
 }
